9242a7395645d137caf7f0763280ed19c9b824f6
lhzandyqh <18627541631@163.com>
2019-09-03 14:57:01 +0800

    20190903到yangqihang分支

---
+++ src/components/myFavoritePage.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="app-container">
+    <el-container style="width: 100%">
+      <el-header height="80px">
+        <el-row>
+          <el-col :span="4" :offset="4">
+            <div class="logo_container">
+              <el-image
+                style="width: 162px; height: 42px"
+                :src=logo></el-image>
+            </div>
+          </el-col>
+          <el-col :span="2" :offset="12">
+            <div class="tubiao-contianer">
+              <div class="tubiao">
+                <!--                <i class="el-icon-s-data"></i>-->
+              </div>
+              <div class="title">
+                <!--                <span style="color: dimgrey;cursor: pointer" @click="goCompositionContent">智能测评</span>-->
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div class="tubiao-contianer">
+              <div class="tubiao">
+                <!--                <i class="el-icon-s-custom"></i>-->
+              </div>
+              <div class="title">
+                <!--                <span v-if="loginFlag==='否'" style="color: dimgrey;cursor: pointer" @click="showLogin">登录</span>-->
+                <!--                <span v-else style="color: dimgrey">我的</span>-->
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <div class="favoriteContainer">
+          <div class="cardContaienr">
+            <el-card class="box-card" style="width: 80%;height: 600px">
+              <el-row>
+                <span style="font-weight: bolder">我的收藏</span>
+              </el-row>
+              <el-divider></el-divider>
+              <el-row>
+                <div v-for="item in compositionData" :key="item.id" class="text item" v-loading="loading">
+                  <el-card style="width: 90%;height: 200px">
+                    <el-row>
+                      <el-col :span="9">
+                        <div class="picture-container">
+                          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image" style="width: 300px;height: 160px">
+                        </div>
+                      </el-col>
+                      <el-col :span="15">
+                        <el-row style="padding-top: 0px">
+                          <el-col :span="24">
+                            <div class="one_row">
+                              <div class="biaoqian">
+                                <el-tag>{{item.ranking}}</el-tag>
+                              </div>
+                              <div class="composition_title">
+                                <span style="font-size: 22px">{{item.title}}</span>
+                              </div>
+                            </div>
+                          </el-col>
+                        </el-row>
+                        <el-row>
+                          <el-col :span="4">
+                            <div class="one_row">
+                              <div class="biaoqian">
+                                <i class="el-icon-time"></i>
+                              </div>
+                              <div class="composition_title">
+                                <span style="font-size: 10px;font-weight: bolder">{{item.year}}</span>
+                              </div>
+                            </div>
+                          </el-col>
+                          <el-col :span="8">
+                            <div class="one_row">
+                              <div class="biaoqian">
+                                <i class="el-icon-view"></i>
+                              </div>
+                              <div class="composition_title">
+                                <span style="font-size: 10px;font-weight: bolder">2019-10-08</span>
+                              </div>
+                            </div>
+                          </el-col>
+                        </el-row>
+                        <el-row style="padding-top: 10px">
+                          <span>{{item.summary}}</span>
+                        </el-row>
+                      </el-col>
+                    </el-row>
+                  </el-card>
+                </div>
+              </el-row>
+              <el-row>
+                <div class="fenye">
+                  <el-pagination
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-sizes="[5]"
+                    :page-size="100"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total=total>
+                  </el-pagination>
+                </div>
+              </el-row>
+            </el-card>
+          </div>
+        </div>
+      </el-main>
+      <el-footer>
+        <div class="copyrightContainer">
+          <span style="font-size: 12px">Copyright&nbsp;&nbsp;&nbsp;中科智禾教育大数据中心   ©2019</span>
+        </div>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import logo from '@/assets/image/logo.png'
+export default {
+  name: 'compositionContent',
+  data () {
+    return {
+      dialogVisible: false,
+      logo: logo,
+      textarea1: '',
+      textarea2: ''
+    }
+  },
+  methods: {
+    startEvaluation: function () {
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .logo_container{
+    padding-top: 20px;
+  }
+  .el-icon-s-custom,
+  .el-icon-s-data{
+    padding-top: 25px;
+    font-size: 23px;
+  }
+  .tubiao-contianer{
+    display: inline-block;
+  }
+  .tubiao{
+    display: inline-block;
+  }
+  .title{
+    display: inline-block;
+  }
+  .el-main{
+    background-color: #F5F5F5;
+  }
+  .copyrightContainer{
+    padding-top: 20px;
+    text-align: center;
+  }
+  .title_container{
+    display: inline-block;
+  }
+  .input{
+    display: inline-block;
+  }
+  .button_container{
+    padding-top: 25px;
+    text-align: center;
+  }
+  .favoriteContainer{
+  }
+  .cardContaienr{
+    padding-left: 15%;
+  }
+  .fenye{
+    text-align: center;
+  }
+
+</style>
---
+++ src/components/user/userPopover.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="appContainer">
+    <el-popover
+      placement="bottom"
+      width="200"
+      trigger="hover">
+      <div class="content">
+        <span style="font-weight: bolder">当前用户：</span>
+        <!--        <span>{{this.toPopoverData}}</span>-->
+        <span style="font-weight: bolder"></span>
+        <el-divider style="padding-top: 10px"></el-divider>
+        <div class="function_item">
+          <div class="functionLogo">
+            <i class="el-icon-star-on"></i>
+          </div>
+          <div class="functionTitle">
+            <el-button type="text" @click="toFavorite" >我的收藏</el-button>
+          </div>
+        </div>
+        <div class="function_item">
+          <div class="functionLogo">
+            <i class="el-icon-unlock"></i>
+          </div>
+          <div class="functionTitle">
+            <el-button type="text" >修改密码</el-button>
+          </div>
+        </div>
+        <div class="function_item">
+          <div class="functionLogo">
+            <i class="el-icon-open"></i>
+          </div>
+          <div class="functionTitle">
+            <el-button type="text" >退出登录</el-button>
+          </div>
+        </div>
+      </div>
+      <span slot="reference">我的</span>
+    </el-popover>
+    <el-dialog
+      title="修改我的密码"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <div class="changepasswordContainer">
+        <el-row :gutter="5">
+          <el-col :span="6">
+            <div class="title">
+              <span style="font-size: 16px;font-weight: bolder">输入我的新密码</span>
+            </div>
+          </el-col>
+          <el-col :span="12" :offset="1">
+            <div class="input">
+              <el-input v-model="password" placeholder="请输入新密码"></el-input>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="5">
+          <el-col :span="6">
+            <div class="title">
+              <span style="font-size: 16px;font-weight: bolder">确认新密码</span>
+            </div>
+          </el-col>
+          <el-col :span="12" :offset="1">
+            <div class="input">
+              <el-input v-model="confirmpassword" placeholder="请确认新密码"></el-input>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary">修改</el-button>
+  </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userPopover',
+  data () {
+    return {
+      confirmpassword: '',
+      password: '',
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleClose: function () {
+      console.log()
+    },
+    toFavorite: function () {
+      let routeData = this.$router.resolve({ path: '/myfavorite' })
+      window.open(routeData.href, '_blank')
+    }
+  }
+  // mounted () {
+  //   this.showStudentName()
+  // },
+  // data () {
+  //   return {
+  //     dialogVisible: false,
+  //     studentNumber: window.localStorage.getItem('studentNumber'),
+  //     confirmpassword: '',
+  //     password: '',
+  //     // studentName: window.localStorage.getItem('studentName')
+  //     student: [{
+  //       studentName: {}
+  //     }]
+  //   }
+  // },
+  // methods: {
+  //   showStudentName: function () {
+  //     this.$set(this.student, 0, {studentName: window.localStorage.getItem('studentName')})
+  //     console.log('查看学生要显示的名字数据')
+  //     console.log(this.student)
+  //   },
+  //   logout: function () {
+  //     window.localStorage.clear()
+  //     window.location.href = './teacher.html#/login'
+  //     location.replace('./teacher.html#/login')
+  //     // this.$router.back(-1)
+  //     // this.$router.push('/login')
+  //   },
+  //   handleClose (done) {
+  //     this.$confirm('确认关闭？')
+  //       .then(_ => {
+  //         done()
+  //       })
+  //       .catch(_ => {})
+  //   },
+  //   changeMyPassword: function () {
+  //     this.dialogVisible = true
+  //   },
+  //   confirmToChangePassword: function () {
+  //     if (this.password === '' || this.confirmpassword === '') {
+  //       this.$message.error('密码不能为空')
+  //     } else {
+  //       if (this.password !== this.confirmpassword) {
+  //         this.$message({
+  //           message: '两次密码输入不同',
+  //           type: 'warning'
+  //         })
+  //       } else {
+  //         const prams = {
+  //           username: window.localStorage.getItem('studentNumber'),
+  //           newpassword: this.password,
+  //           assertpassword: this.confirmpassword
+  //         }
+  //         changePassword(prams).then(respone => {
+  //           this.$message({
+  //             message: '修改密码成功,请重新登录',
+  //             type: 'success',
+  //             duration: 5000
+  //           })
+  //           this.dialogVisible = false
+  //           this.logout()
+  //         })
+  //       }
+  //     }
+  //   }
+  // }
+}
+</script>
+
+<style scoped>
+  .content{
+    text-align: center;
+  }
+  .function_item{
+    float: left;
+    display: inline-block;
+  }
+  .functionLogo{
+    float: left;
+    margin-left: 30px;
+  }
+  .functionTitle{
+    float: left;
+    margin-left: 60px;
+    margin-top: -10px;
+  }
+
+</style>
